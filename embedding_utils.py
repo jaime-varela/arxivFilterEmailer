3b7a2337872f290b8197ef92aa37af544d874a27
jaime-varela <varela.jaime01@gmail.com>
2023-05-07 11:14:39 -0400

    added mask to attention

--- embedding_utils.py
+++ embedding_utils.py
@@ -3,11 +3,11 @@ import torch
 def get_mean_pooled_embeddings(sentences,tokenizer,model):
     #Encode all sentences
     tokenized_sentences = tokenizer(sentences, truncation=True, padding=True)
-
-    encoded_layers = model(torch.tensor(tokenized_sentences['input_ids']))
+    tokens_tensor = torch.tensor(tokenized_sentences['input_ids'])
+    attention_mask = torch.tensor(tokenized_sentences['attention_mask'])
+    encoded_layers = model(tokens_tensor,attention_mask=attention_mask)
     embeddings = encoded_layers.last_hidden_state
 
-    attention_mask = torch.tensor(tokenized_sentences['attention_mask'])
     mask = attention_mask.unsqueeze(-1).expand(embeddings.size()).float()
     masked_embeddings = embeddings * mask
 
